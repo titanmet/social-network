61510b6530d2ea1ca67c89bfeb5d2c96c68973b3
titanmet@mail.ru <titanmet@mail.ri>
2020-06-30 08:22:33 +0300

    init

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo08Mi3qmkDV2ivCx-Xt0vZL5LeLAPbVP2yA&usqp=CAU' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+    <div className='content'>
+        <div>
+            <img src='https://gameranx.com/wp-content/uploads/2019/08/Animal-Crossing-New-Horizons-1080P-Wallpaper-2-1024x576.jpg' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my post
+            <div>
+                new post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Profile;
