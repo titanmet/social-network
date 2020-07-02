d7554c3afc0e377f4ce7926baca475039b70cb98
titanmet@mail.ru <titanmet@mail.ri>
2020-07-02 15:56:52 +0300

    route npm i react-router-dom -save

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from 'react-router-dom';
 
 
@@ -16,6 +19,9 @@ const App = () => {
                 <div class='app-wrapper-content'>
                     <Route path='/profile' component={Profile}/>
                     <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,13 +11,13 @@ const Nav = () =>{
                 <a href='/dialogs'> Messages </a>
             </div>
             <div className={nav_classes.item}>
-                <a> News </a>
+                <a href='/news'> News </a>
             </div>
             <div className={nav_classes.item}>
-                <a> Music </a>
+                <a href='/music'> Music </a>
             </div>
             <div className={nav_classes.item}>
-                <a> Settings </a>
+                <a href='/settings'> Settings </a>
             </div>
         </nav>
     )
