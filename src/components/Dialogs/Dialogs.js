ce47516b2bda5e5b28f75e4d231615ac045beee9
titanmet@mail.ru <titanmet@mail.ri>
2020-07-03 14:12:25 +0300

    dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,10 +1,47 @@
 import React from "react";
 import Dialog_classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={Dialog_classes.dialogs}>
+            <div className={Dialog_classes.dialogsItems}>
+                <div className={Dialog_classes.dialog}>
+                   <NavLink to="/dialogs/1"> Artem </NavLink>
+                </div>
+                <div className={Dialog_classes.dialog + ' ' + Dialog_classes.active}>
+                    <NavLink to="/dialogs/2"> Andrey </NavLink>
+                </div>
+                <div className={Dialog_classes.dialog}>
+                    <NavLink to="/dialogs/3"> Sonya </NavLink>
+                </div>
+                <div className={Dialog_classes.dialog}>
+                    <NavLink to="/dialogs/4"> Olya </NavLink>
+                </div>
+                <div className={Dialog_classes.dialog}>
+                    <NavLink to="/dialogs/5"> Alexey </NavLink>
+                </div>
+                <div className={Dialog_classes.dialog}>
+                    <NavLink to="/dialogs/6"> Natasha </NavLink>
+                </div>
+                <div className={Dialog_classes.dialog}>
+                    <NavLink to="/dialogs/7"> Sasha </NavLink>
+                </div>
+                <div className={Dialog_classes.dialog}>
+                    <NavLink to="/dialogs/8"> Ira </NavLink>
+                </div>
+            </div>
+            <div className={Dialog_classes.messages}>
+                <div className={Dialog_classes.message}>
+                    Hi whats'up
+                </div>
+                <div className={Dialog_classes.message}>
+                    How are you
+                </div>
+                <div className={Dialog_classes.message}>
+                    Very nace
+                </div>
+            </div>
         </div>
     )
 }
